078f5997230016c1d41a8be4bd0c4ea81d7d74f0
Antenor <aadjafre@gmail.com>
2018-09-15 12:45:03 -0300

    Creating project

---
+++ src/main/java/com/uni7/takersandgivers/TakersandgiversApplication.java
@@ -0,0 +1,12 @@
+package com.uni7.takersandgivers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakersandgiversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakersandgiversApplication.class, args);
+	}
+}
---
+++ src/test/java/com/uni7/takersandgivers/TakersandgiversApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uni7.takersandgivers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TakersandgiversApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
